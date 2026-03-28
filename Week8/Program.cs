e2c8691e9c112d82c01a56be1a1339aee2198994
nazariidatsenko25-png <nazarii.datsenko.25@kse.org.ua>
2026-03-28 16:28:37 +0200

    commit 3

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,19 @@
-﻿
-class Program
-{
-    public static void Main()
-    {
-        
-    }
-    
-}
+﻿using Week8;
+
+Point p1 = new Point{X = 1, Y = 2};
+Point p2 = p1;
+
+p2.X = 20; 
+p1.Print();
+p2.Print();
+
+Console.WriteLine();
+PointRef r1  = new PointRef{X = 1, Y = 2};
+PointRef r2 = r1;
+r2.X = 100;
+r1.Print();
+r2.Print();
+Console.WriteLine();
+
+BoxingTester.Test();
+Console.WriteLine();
