cbcd252c402da1d528552c3e3162eef3bbe00698
nazariidatsenko25-png <nazarii.datsenko.25@kse.org.ua>
2026-03-28 16:08:09 +0200

    commit 3

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,15 @@
-﻿
-class Program
+﻿using Week8;
+
+var file = new FileResource("report.txt");
+var network = new NetworkResource("api.company.local");
+var manager  = new ResourceManager<Resource>();
+
+manager.Add(file);
+manager.Add(network);
+manager.OpenAll();
+manager.CloseAll();
+using (var res = new FileResource("report.txt"))
 {
-    public static void Main()
-    {
-        
-    }
-    
+    res.Open();
 }
+Console.WriteLine("Done");
