a423dded753ff9a448d5f43a61e0ac9c2fbe7bb8
nazariidatsenko25-png <nazarii.datsenko.25@kse.org.ua>
2026-03-28 16:47:36 +0200

    commit 3

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,18 @@
-﻿
-class Program
+﻿using Week8;
+var container  = new DataContainer<DataItem>();
+for (int i = 0; i < 10000;  i++)
 {
-    public static void Main()
+    var item = new DataItem
     {
-        
-    }
-    
+        Id = $"item{i}",
+        Payload = new byte[1024]
+    };
+    container.Add(item);
 }
+Console.WriteLine($"total size before GC: {container.GetTotalSize()}");
+GC.Collect();
+Console.WriteLine("GC.Collect() was called ");
+Console.WriteLine($"total size after GC: {container.GetTotalSize()}");
+
+Console.WriteLine("total size did not changed after calling GC.Collect(), that happened because objects still holding active reference");
+
